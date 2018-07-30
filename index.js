5024d34d724549da8323899fa6ab9b16e3c5e680
Emily Asch <e.asch@me.com>
2018-07-30 21:36:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments){
+    var array = [];
+    for (var i = 0; i < musicians.length; i++){
+      array.push (musicians[i] + " " + "plays" + " "+ instruments[i]);
+    }
+    return array
+}
+
+function johnLennonFacts (facts){
+  var newfacts = [];
+  var i = 0 
+  while (i < facts.length){
+    newfacts.push(facts[i] + "!!!")
+    i++;
+  }
+  return newfacts;
+}
